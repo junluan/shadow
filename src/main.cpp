ce2a5ae85cf123307109f31d28dd34797a97725e
jun@work <mutate@aliyun.com>
2016-04-21 17:21:41 +0800

    rename directory include to inc, update main and CmakeLists.

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,18 @@
 int main(int argc, char const *argv[]) {
 
   Yolo yolo("./cfg/yolo-refine.conv_adas.json",
-            "./models/yolo-refine_10000.weights", 0.2);
+            "./models/yolo-refine_final.weights", 0.2);
 
+  Box roi;
+  roi.x = 700;
+  roi.y = 500;
+  roi.w = 660;
+  roi.h = 480;
   yolo.Setup();
-  //yolo.Test("./data/demo_5.png");
-  // yolo.BatchTest("./data/testlist.txt", false);
-   //yolo.VideoTest("./data/set07V000.avi", true);
-   yolo.Demo(0, false);
+  yolo.Test("./data/demo_6.png");
+  //yolo.BatchTest("./data/testlist.txt", true);
+  //yolo.VideoTest("./data/traffic/set07V000.avi", true, true);
+  //yolo.Demo(0, true);
   yolo.Release();
   return 0;
 }
