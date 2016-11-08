e6d9aa91432aca712f1384b0780ac69ae41b9194
jun@windows <mutate@aliyun.com>
2016-11-08 21:47:00 +0800

    refactor code: refactor windows solution

--- windows/scripts/CopyDependencies.cmd
+++ windows/scripts/CopyDependencies.cmd
@@ -2,6 +2,6 @@ set DLL_Path=%~1%
 set OUT_DIR=%~2%
 set DLLs=%~3%
 
-echo copy "%DLLs%" dlls to "%OUT_DIR%"
+echo CopyDependencies.cmd : Copy "%DLLs%" dlls to "%OUT_DIR%"
 robocopy /NS /NC /NFL /NDL /NP /NJH /NJS "%DLL_Path%" %OUT_DIR% %DLLs%
 if errorlevel 1 (exit 0)
