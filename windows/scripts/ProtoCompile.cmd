6f8bf37451af9d4194cb02bf861eb482bce3a880
jun@windows <mutate@aliyun.com>
2016-12-28 10:53:26 +0800

    add feature: update windows solution for batch norm layer, scale layer and add test_shadow project

--- windows/scripts/ProtoCompile.cmd
+++ windows/scripts/ProtoCompile.cmd
@@ -12,15 +12,22 @@ echo ProtoCompile.cmd : Generating .cc and .h files for "%SHADOW_PROTO_DIR%\shad
 "%PROTOC_DIR%\protoc" --proto_path="%SHADOW_PROTO_DIR%" --cpp_out="%PROTO_TEMP_DIR%" "%SHADOW_PROTO_DIR%\shadow.proto"
 "%PROTOC_DIR%\protoc" --proto_path="%CAFFE_PROTO_DIR%" --cpp_out="%PROTO_TEMP_DIR%" "%CAFFE_PROTO_DIR%\caffe.proto"
 
-echo ProtoCompile.cmd : Compare newly compiled shadow.ph.h and caffe.pb.h with existing ones
+echo ProtoCompile.cmd : Compare newly compiled shadow.ph.h with existing one
 fc /b "%PROTO_TEMP_DIR%\shadow.pb.h" "%SHADOW_PROTO_DIR%\shadow.pb.h" > NUL
-fc /b "%PROTO_TEMP_DIR%\caffe.pb.h" "%CAFFE_PROTO_DIR%\caffe.pb.h" > NUL
 
 if errorlevel 1 (
   robocopy /NS /NC /NFL /NDL /NP /NJH /NJS "%PROTO_TEMP_DIR%" %SHADOW_PROTO_DIR% shadow.pb.cc shadow.pb.h
+)
+
+echo ProtoCompile.cmd : Compare newly compiled caffe.pb.h with existing one
+fc /b "%PROTO_TEMP_DIR%\caffe.pb.h" "%CAFFE_PROTO_DIR%\caffe.pb.h" > NUL
+
+if errorlevel 1 (
   robocopy /NS /NC /NFL /NDL /NP /NJH /NJS "%PROTO_TEMP_DIR%" %CAFFE_PROTO_DIR% caffe.pb.cc caffe.pb.h
 )
 
 rmdir /S /Q "%PROTO_TEMP_DIR%"
 
-if errorlevel 1 (set errorlevel=0)
+if errorlevel 1 (
+  set errorlevel=0
+)
