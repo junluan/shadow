ae1eab919138869ad49643f0d7142aaad04f07b8
jun@work <mutate@aliyun.com>
2017-04-19 13:31:15 +0800

    add feature: add sort boxes by scores in nms

--- src/shadow/util/boxes.cpp
+++ src/shadow/util/boxes.cpp
@@ -42,14 +42,27 @@ float IoU(const Box<Dtype> &box_a, const Box<Dtype> &box_b) {
 }
 
 template <typename Dtype>
+inline bool SortBoxesAscend(const Box<Dtype> &box_a, const Box<Dtype> &box_b) {
+  return box_a.score < box_b.score;
+}
+
+template <typename Dtype>
+inline bool SortBoxesDescend(const Box<Dtype> &box_a, const Box<Dtype> &box_b) {
+  return box_a.score > box_b.score;
+}
+
+template <typename Dtype>
 std::vector<Box<Dtype>> NMS(const std::vector<std::vector<Box<Dtype>>> &Bboxes,
                             float iou_threshold) {
   std::vector<Box<Dtype>> all_boxes;
   for (const auto &boxes : Bboxes) {
     for (const auto &box : boxes) {
-      if (box.label != -1) all_boxes.push_back(box);
+      if (box.label != -1) {
+        all_boxes.push_back(box);
+      }
     }
   }
+  std::stable_sort(all_boxes.begin(), all_boxes.end(), SortBoxesDescend<Dtype>);
   for (int i = 0; i < all_boxes.size(); ++i) {
     Box<Dtype> &box_i = all_boxes[i];
     if (box_i.label == -1) continue;
@@ -63,15 +76,16 @@ std::vector<Box<Dtype>> NMS(const std::vector<std::vector<Box<Dtype>>> &Bboxes,
         continue;
       }
       float in = Intersection(box_i, box_j);
-      float cover_i = in / Size(box_i);
-      float cover_j = in / Size(box_j);
+      float cover_i = in / Size(box_i), cover_j = in / Size(box_j);
       if (cover_i > cover_j && cover_i > 0.7) box_i.label = -1;
       if (cover_i < cover_j && cover_j > 0.7) box_j.label = -1;
     }
   }
   std::vector<Box<Dtype>> out_boxes;
   for (const auto &box : all_boxes) {
-    if (box.label != -1) out_boxes.push_back(box);
+    if (box.label != -1) {
+      out_boxes.push_back(box);
+    }
   }
   all_boxes.clear();
   return out_boxes;
