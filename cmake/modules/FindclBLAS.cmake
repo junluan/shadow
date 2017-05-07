bbad42f38789f2f0eb740dc848bbf6f74e420003
jun@windows <mutate@aliyun.com>
2017-05-07 17:24:49 +0800

    add feature: add cmake opencl support for windows

---
+++ cmake/modules/FindclBLAS.cmake
@@ -0,0 +1,33 @@
+include(FindPackageHandleStandardArgs)
+
+set(clBLAS_ROOT_DIR ${PROJECT_SOURCE_DIR}/third_party/clblas CACHE PATH "Folder contains clBLAS")
+
+set(clBLAS_DIR ${clBLAS_ROOT_DIR} /usr /usr/local)
+
+find_path(clBLAS_INCLUDE_DIRS
+          NAMES clBLAS.h
+          PATHS ${clBLAS_DIR}
+          PATH_SUFFIXES include include/x86_64 include/x64
+          DOC "clBLAS include header clBLAS.h"
+          NO_DEFAULT_PATH)
+
+find_library(clBLAS_LIBRARIES
+             NAMES clBLAS
+             PATHS ${clBLAS_DIR}
+             PATH_SUFFIXES lib lib64 lib/x86_64 lib/x64 lib/x86
+             DOC "clBLAS library"
+             NO_DEFAULT_PATH)
+
+find_package_handle_standard_args(clBLAS DEFAULT_MSG clBLAS_INCLUDE_DIRS clBLAS_LIBRARIES)
+
+if (clBLAS_FOUND)
+  if (NOT clBLAS_FIND_QUIETLY)
+    message(STATUS "Found clBLAS include: ${clBLAS_INCLUDE_DIRS}")
+    message(STATUS "Found clBLAS libraries: ${clBLAS_LIBRARIES}")
+  endif ()
+  mark_as_advanced(clBLAS_ROOT_DIR clBLAS_INCLUDE_DIRS clBLAS_LIBRARIES)
+else ()
+  if (clBLAS_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find clBLAS")
+  endif ()
+endif ()
