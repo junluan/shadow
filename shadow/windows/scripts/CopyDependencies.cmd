a553cd36ab34a379f62df3b85af6ad944cddf53e
jun@notebook <mutate@aliyun.com>
2017-07-02 16:54:43 +0800

    refactor code: delete windows solution files, use cmake to generate them

--- shadow/windows/scripts/CopyDependencies.cmd
+++
@@ -1,7 +0,0 @@
-set DLL_Path=%~1%
-set OUT_DIR=%~2%
-set DLLs=%~3%
-
-echo CopyDependencies.cmd : Copy "%DLLs%" dlls to "%OUT_DIR%"
-robocopy /NS /NC /NFL /NDL /NP /NJH /NJS "%DLL_Path%" %OUT_DIR% %DLLs%
-if errorlevel 1 (set errorlevel=0)
