185b580c5548a0b0b423aff15f4005ad6b9b8431
jun@macbook <mutate@aliyun.com>
2018-02-22 12:08:42 +0800

    add feature: cmake support find eigen3 installed by homebrew

--- cmake/modules/FindEigen.cmake
+++ cmake/modules/FindEigen.cmake
@@ -7,7 +7,7 @@ set(Eigen_DIR ${Eigen_ROOT_DIR} /usr /usr/local)
 find_path(Eigen_INCLUDE_DIRS
           NAMES Eigen/Eigen
           PATHS ${Eigen_DIR}
-          PATH_SUFFIXES include include/x86_64 include/x64
+          PATH_SUFFIXES include include/x86_64 include/x64 include/eigen3
           DOC "Eigen include header"
           NO_DEFAULT_PATH)
 
