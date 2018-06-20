76f253412f5a0ea62cf71b1c00e0d90fcb077fa2
jun@macbook <mutate@aliyun.com>
2018-06-20 13:07:00 +0800

    add feature: update nnpack; enable nnpack in travis

--- cmake/external/DownloadNNPACK.cmake
+++ cmake/external/DownloadNNPACK.cmake
@@ -6,11 +6,12 @@ include(ExternalProject)
 
 ExternalProject_Add(nnpack
                     GIT_REPOSITORY "https://github.com/Maratyszcza/NNPACK.git"
-                    GIT_TAG "master"
+                    GIT_TAG "af40ea7d12702f8ae55aeb13701c09cad09334c3"
                     SOURCE_DIR "${PROJECT_SOURCE_DIR}/third_party/nnpack"
                     BINARY_DIR "${CMAKE_BINARY_DIR}/nnpack-build"
                     CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/third_party/nnpack/build"
                                "-DCMAKE_BUILD_TYPE=Release"
+                               "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
                                "-DNNPACK_LIBRARY_TYPE=static"
                                "-DNNPACK_BUILD_TESTS=OFF"
                     BUILD_COMMAND "${CMAKE_COMMAND}" --build . --target install
