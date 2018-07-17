51ed7cee0fc58c17fd28cff074a797c29ff96732
jun@macbook <mutate@aliyun.com>
2018-07-17 21:40:55 +0800

    fix bug: use specific eigen

--- cmake/external/DownloadEigen.cmake
+++ cmake/external/DownloadEigen.cmake
@@ -6,7 +6,7 @@ include(ExternalProject)
 
 ExternalProject_Add(eigen
                     GIT_REPOSITORY "https://github.com/eigenteam/eigen-git-mirror.git"
-                    GIT_TAG "master"
+                    GIT_TAG "3.3.4"
                     SOURCE_DIR "${PROJECT_SOURCE_DIR}/third_party/eigen3"
                     BINARY_DIR "${CMAKE_BINARY_DIR}/eigen-build"
                     CONFIGURE_COMMAND ""
