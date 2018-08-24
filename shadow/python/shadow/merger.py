a4c0f79837103088396baf93413693a0f336963a
jun@macbook <mutate@aliyun.com>
2018-08-24 21:13:18 +0800

    add feature: support merge bn after deconv

--- shadow/python/shadow/merger.py
+++ shadow/python/shadow/merger.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 from __future__ import division
 
-import copy
-import math
+import numpy as np
 from shadow.net_spec import Shadow
 
 
@@ -39,8 +38,8 @@ def convert_batch_norm(o, ori_net, merged_net, copy_params):
     merged_op = merged_net.add_op()
     merged_op.name = conv_op.name
     merged_op.type = conv_op.type
-    merged_op.top.extend(scale_op.top)
     merged_op.bottom.append(conv_op.bottom[0])
+    merged_op.top.extend(scale_op.top)
 
     for arg in conv_op.arg:
         if arg.name != 'bias_term':
@@ -66,45 +65,51 @@ def convert_batch_norm(o, ori_net, merged_net, copy_params):
     assert out_c > 0
     merge_bias_blob.shape.append(out_c)
 
-    weight = copy.deepcopy(conv_weight_blob.data_f)
+    weight = np.asarray(conv_weight_blob.data_f)
     if get_arg(conv_op, 'bias_term', 's_i', 1):
         conv_bias_blob = get_blob(ori_net, conv_op.bottom[2])
         assert conv_bias_blob is not None
-        bias = copy.deepcopy(conv_bias_blob.data_f)
+        bias = np.asarray(conv_bias_blob.data_f)
     else:
-        bias = [0] * out_c
+        bias = np.zeros(out_c)
 
     eps = get_arg(bn_op, 'eps', 's_f', 1e-5)
     bn_mean_blob = get_blob(ori_net, bn_op.bottom[1])
     bn_var_blob = get_blob(ori_net, bn_op.bottom[2])
     assert bn_mean_blob is not None and bn_var_blob is not None
-    bn_mean = copy.deepcopy(bn_mean_blob.data_f)
-    bn_var = copy.deepcopy(bn_var_blob.data_f)
-
+    bn_mean = np.asarray(bn_mean_blob.data_f)
+    bn_var = np.asarray(bn_var_blob.data_f)
     bn_scale = 1
     if len(bn_op.bottom) == 4:
         bn_scale_blob = get_blob(ori_net, bn_op.bottom[3])
         assert bn_scale_blob is not None
         bn_scale = bn_scale_blob.data_f[0]
-
-    if bn_scale != 0:
-        bn_scale = 1. / bn_scale
-    for i in range(0, len(bn_mean)):
-        bn_mean[i] *= bn_scale
-        bn_var[i] = 1. / math.sqrt(abs(bn_var[i]) * bn_scale + eps)
+        bn_scale = 1. / bn_scale if bn_scale != 0 else bn_scale
 
     scale_scale_blob = get_blob(ori_net, scale_op.bottom[1])
     scale_bias_blob = get_blob(ori_net, scale_op.bottom[2])
-    assert scale_scale_blob is not None
-    assert scale_bias_blob is not None
-    scale_scale = copy.deepcopy(scale_scale_blob.data_f)
-    scale_bias = copy.deepcopy(scale_bias_blob.data_f)
-    spatial = int(len(weight) / out_c)
-    for i in range(0, len(weight)):
-        c = int(i / spatial)
-        weight[i] *= bn_var[c] * scale_scale[c]
-    for i in range(0, len(bias)):
-        bias[i] = (bias[i] - bn_mean[i]) * bn_var[i] * scale_scale[i] + scale_bias[i]
+    assert scale_scale_blob is not None and scale_bias_blob is not None
+    scale_scale = np.asarray(scale_scale_blob.data_f)
+    scale_bias = np.asarray(scale_bias_blob.data_f)
+
+    bn_mean *= bn_scale
+    bn_var = 1. / np.sqrt(np.absolute(bn_var) * bn_scale + eps)
+
+    num_weight = len(weight)
+    if merged_op.type == 'Conv':
+        assert num_weight % out_c == 0
+        spatial = int(num_weight / out_c)
+        weight *= bn_var.repeat(spatial) * scale_scale.repeat(spatial)
+    elif merged_op.type == 'Deconv':
+        kernel_size = get_arg(conv_op, 'kernel_size', 's_i', 0)
+        assert kernel_size > 0
+        kernel_spatial = kernel_size * kernel_size
+        assert num_weight % (out_c * kernel_spatial) == 0
+        in_c = int(num_weight / (out_c * kernel_spatial))
+        weight *= np.tile(bn_var.repeat(kernel_spatial), in_c) * np.tile(scale_scale.repeat(kernel_spatial), in_c)
+    else:
+        raise ValueError('Unknown op type', merged_op.type)
+    bias = (bias - bn_mean) * bn_var * scale_scale + scale_bias
 
     merge_weight_blob.data_f.extend(weight)
     merge_bias_blob.data_f.extend(bias)
@@ -135,7 +140,7 @@ def merge_batchnorm(shadow_net, copy_params):
         o = 0
         while o < op_size:
             op = ori_net.op[o]
-            if 'Conv' in op.type:
+            if op.type == 'Conv' or op.type == 'Deconv':
                 bn_index, scale_index = o + 1, o + 2
                 has_bn, has_scale = False, False
                 if bn_index < op_size:
@@ -166,7 +171,7 @@ def merge_activate(shadow_net):
         o = 0
         while o < op_size:
             op = ori_net.op[o]
-            if 'Conv' in op.type:
+            if op.type == 'Conv' or op.type == 'Deconv':
                 ac_index = o + 1
                 has_ac = False
                 if ac_index < op_size:
