a108d941a1b8c1121651d797ac3393a3575d2f16
jun@macbook <mutate@aliyun.com>
2019-05-06 15:42:32 +0800

    add feature: refine find protobuf cmake

--- cmake/modules/FindProtobuf.cmake
+++ cmake/modules/FindProtobuf.cmake
@@ -4,12 +4,6 @@ set(Protobuf_ROOT_DIR ${PROJECT_SOURCE_DIR}/third_party/protobuf CACHE PATH "Fol
 
 set(Protobuf_DIR ${Protobuf_ROOT_DIR} /usr /usr/local)
 
-find_program(Protoc_EXECUTABLE
-             NAMES protoc
-             PATHS ${Protobuf_DIR}
-             PATH_SUFFIXES bin
-             NO_DEFAULT_PATH)
-
 find_path(Protobuf_INCLUDE_DIRS
           NAMES google/protobuf/message.h
           PATHS ${Protobuf_DIR}
@@ -22,22 +16,20 @@ find_library(Protobuf_LIBRARIES
              PATH_SUFFIXES lib lib64 lib/x86_64 lib/x86_64-linux-gnu lib/x64 lib/x86
              NO_DEFAULT_PATH)
 
+find_program(Protoc_EXECUTABLE
+             NAMES protoc
+             PATHS ${Protobuf_DIR}
+             PATH_SUFFIXES bin
+             NO_DEFAULT_PATH)
+
 find_package_handle_standard_args(Protobuf DEFAULT_MSG Protobuf_INCLUDE_DIRS Protobuf_LIBRARIES Protoc_EXECUTABLE)
 
 if (Protobuf_FOUND)
-  set(Protobuf_VERSION "")
-  set(Protobuf_LIB_VERSION "")
-  set(PROTOBUF_COMMON_FILE ${Protobuf_INCLUDE_DIRS}/google/protobuf/stubs/common.h)
-  file(STRINGS ${PROTOBUF_COMMON_FILE}
-       PROTOBUF_COMMON_H_CONTENTS
-       REGEX "#define[ \t]+GOOGLE_PROTOBUF_VERSION[ \t]+")
-  if (PROTOBUF_COMMON_H_CONTENTS MATCHES "#define[ \t]+GOOGLE_PROTOBUF_VERSION[ \t]+([0-9]+)")
-    set(Protobuf_LIB_VERSION "${CMAKE_MATCH_1}")
-  endif ()
-  unset(PROTOBUF_COMMON_H_CONTENTS)
-  math(EXPR PROTOBUF_MAJOR_VERSION "${Protobuf_LIB_VERSION} / 1000000")
-  math(EXPR PROTOBUF_MINOR_VERSION "${Protobuf_LIB_VERSION} / 1000 % 1000")
-  math(EXPR PROTOBUF_SUBMINOR_VERSION "${Protobuf_LIB_VERSION} % 1000")
+  parse_header(${Protobuf_INCLUDE_DIRS}/google/protobuf/stubs/common.h
+               GOOGLE_PROTOBUF_VERSION)
+  math(EXPR PROTOBUF_MAJOR_VERSION "${GOOGLE_PROTOBUF_VERSION} / 1000000")
+  math(EXPR PROTOBUF_MINOR_VERSION "${GOOGLE_PROTOBUF_VERSION} / 1000 % 1000")
+  math(EXPR PROTOBUF_SUBMINOR_VERSION "${GOOGLE_PROTOBUF_VERSION} % 1000")
   set(Protobuf_VERSION "${PROTOBUF_MAJOR_VERSION}.${PROTOBUF_MINOR_VERSION}.${PROTOBUF_SUBMINOR_VERSION}")
   execute_process(COMMAND ${Protoc_EXECUTABLE} --version OUTPUT_VARIABLE Protoc_VERSION)
   if ("${Protoc_VERSION}" MATCHES "libprotoc ([0-9.]+)")
@@ -50,7 +42,7 @@ if (Protobuf_FOUND)
     message(STATUS "Found Protobuf: ${Protobuf_INCLUDE_DIRS}, ${Protobuf_LIBRARIES} (found version ${Protobuf_VERSION})")
     message(STATUS "Found Protoc: ${Protoc_EXECUTABLE} (found version ${Protoc_VERSION})")
   endif ()
-  mark_as_advanced(Protobuf_ROOT_DIR Protobuf_INCLUDE_DIRS Protobuf_LIBRARIES Protobuf_LIBRARIES_RELEASE Protobuf_LIBRARIES_DEBUG Protoc_EXECUTABLE)
+  mark_as_advanced(Protobuf_ROOT_DIR Protobuf_INCLUDE_DIRS Protobuf_LIBRARIES Protoc_EXECUTABLE)
 else ()
   if (Protobuf_FIND_REQUIRED)
     message(FATAL_ERROR "Could not find Protobuf")
