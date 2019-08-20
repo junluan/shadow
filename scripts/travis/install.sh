f201631e9317a32f825cf397a8f2286f4c6a349c
jun@macbook <mutate@aliyun.com>
2019-08-20 16:35:10 +0800

    add feature: update travis linux to ubuntu 16.04

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -3,15 +3,15 @@
 set -ex
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    CUDA_REPO_PKG="cuda-repo-ubuntu1404_10.0.130-1_amd64.deb"
+    CUDA_REPO_PKG="cuda-repo-ubuntu1604_10.0.130-1_amd64.deb"
     CUDA_PKG_VERSION="10-0"
     CUDA_VERSION="10.0"
-    wget "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG"
+    wget "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/$CUDA_REPO_PKG"
     sudo dpkg -i "$CUDA_REPO_PKG"
 
-    CUDNN_REPO_PKG="nvidia-machine-learning-repo-ubuntu1404_4.0-2_amd64.deb"
+    CUDNN_REPO_PKG="nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb"
     CUDNN_PKG_VERSION="7.6.2.24-1+cuda10.0"
-    wget "https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64/$CUDNN_REPO_PKG"
+    wget "https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/$CUDNN_REPO_PKG"
     sudo dpkg -i "$CUDNN_REPO_PKG"
 
     sudo apt-get update
